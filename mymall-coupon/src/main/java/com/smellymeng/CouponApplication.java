6f699bf25552456862a2f1dce08dbd115859db4d
smellymeng <576245190@smellymeng.qq.com>
2022-09-03 19:37:07 +0800

    first construct

--- mymall-coupon/src/main/java/com/smellymeng/CouponApplication.java
+++ mymall-coupon/src/main/java/com/smellymeng/CouponApplication.java
@@ -1,11 +1,17 @@
 package com.smellymeng;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.smellymeng.mymall.coupon.dao")
 public class CouponApplication {
 
+    /*
+    * 整合mybatis-plus
+    *
+    * */
     public static void main(String[] args) {
 
         SpringApplication.run(CouponApplication.class,args);
